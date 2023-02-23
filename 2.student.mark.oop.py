1a0ef2513623c49c614b064777c58dcdeb399eb1
j1nxie <rylieeeeexd@gmail.com>
2023-02-23 13:59:37 +0700

    feat: `Course` class impl

--- 2.student.mark.oop.py
+++ 2.student.mark.oop.py
@@ -3,6 +3,7 @@ import os
 import textwrap
 from typing import Self
 
+
 class Student:
     def __init__(self, name: str, id: int, dob: str) -> Self:
         self.__name = name
@@ -29,6 +30,34 @@ class Student:
 
     def set_dob(self, dob: str) -> Self:
         self.__dob = dob
+        return self
+
+class Course:
+    def __init__(self, name: str, id: int, marks: list) -> Self:
+        self.__name = name
+        self.__id = id
+        self.__marks = marks
+        return self
+
+    def get_name(self) -> str:
+        return self.__name
+
+    def get_id(self) -> int:
+        return self.__id
+
+    def get_marks(self) -> list:
+        return self.__marks
+
+    def set_name(self, name: str) -> Self:
+        self.__name = name
+        return self
+
+    def set_id(self, id: int) -> Self:
+        self.__id = id
+        return self
+
+    def set_marks(self, marks: list) -> Self:
+        self.__marks = marks
         return self
 
 def main():
