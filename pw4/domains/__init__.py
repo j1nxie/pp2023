3be79ffa9bccaab5a0434145a5a07f45c1a86466
j1nxie <rylieeeeexd@gmail.com>
2023-03-27 09:48:31 +0700

    style: sort imports alphabetically

--- pw4/domains/__init__.py
+++ pw4/domains/__init__.py
@@ -1,6 +1,7 @@
-from course import Course
-from eduobj import EduObj
-from mark import Mark
-from student import Student
+from .course import Course
+from .eduobj import EduObj
+from .major import Major
+from .mark import Mark
+from .student import Student
 
-__all__ = ["EduObj", "Student", "Course", "Mark"]
+__all__ = ["Course", "EduObj", "Major", "Mark", "Student"]
